7a0881c7c71d579f722f7da96055f7d0a7f6010e
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-05-05 08:32:03 -0700

    Add function to put the number in an array to create a range.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
+var put_in_array = function(number){
+  var array = [];
+  
+  for(var i = 0; i < number; i++){
+    array.push(i + 1);
+  }
+  return array;
+}
+
 $(document).ready(function(){
   $("form#mainform").submit(function(event){
-    
+    event.preventDefault();
+    var number = $("input#number").val()
+
+    var array = put_in_array(number);
   });
 });
