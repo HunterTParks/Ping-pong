37bd3cbf73a6b58080ceedcd02938f7f0f773389
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-05-05 08:49:12 -0700

    Add function that checks to see if a number in the range can be divisible by 5. If so, Change that number to PONG

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,20 @@ var put_in_array = function(number){
   }
   return array;
 }
+
 var divisible_by_three = function(array){
   for(var i = 0; i < array.length; i++){
     if(array[i] % 3 === 0){
       array[i] = "PING";
+    }
+  }
+  return array;
+}
+
+var divisible_by_five = function(array){
+  for(var i = 0; i < array.length; i++){
+    if(array[i] % 5 === 0){
+      array[i] = "PONG";
     }
   }
   console.log(array);
@@ -22,5 +32,6 @@ $(document).ready(function(){
 
     var array = put_in_array(number);
     array = divisible_by_three(array);
+    array = divisible_by_five(array);
   });
 });
